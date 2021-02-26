b88764ad9c3ffd5484d4731b2d427e12d01fe798
Hiro <hiromichi.s.617@gmail.com>
2021-02-26 14:49:15 +0900

    100%

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
       <main className={css`
         width: 100%;
-        height: 100vh;
+        height: 100%;
         display: flex;
         justify-content: center;
         align-items: center;
